e22fd74f2b60a2927166eb39a83974899fb85001
Djamel <djamel.ammour@gmail.com>
2019-04-09 13:36:34 +0200

    initialize app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  GenerateEmployee  from  './components/GenerateEmployee';
+import  DisplayEmployee  from  './components/DisplayEmployee';
 
 class App extends Component {
   render() {
+    const  sampleEmployee = {
+      gender:  'male',
+      name: {
+          title:  'mr',
+          first:  'mathys',
+          last:  'aubert'
+      },
+      location: {
+          street:  '9467 rue gasparin',
+          city:  'perpignan',
+          postcode:  '90208'
+      },
+      email:  'mathys.aubert@example.com',
+      picture: {
+          medium:  'https://randomuser.me/api/portraits/med/men/66.jpg'
+      }
+  };
     return (
       <div className="App">
         <header className="App-header">
@@ -20,6 +39,8 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <GenerateEmployee  />
+        <DisplayEmployee  employee={sampleEmployee}  />
       </div>
     );
   }
